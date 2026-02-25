072ef5d197ed2124fb8ac9d50bcbed25f85f7b37
Israel Amaya <183645151+Israel-engineer@users.noreply.github.com>
2026-02-24 19:54:45 -0500

    agregue varias lineas donde se consulta el tipo de  dato utilizando la funcion type.

--- hello.py
+++ hello.py
@@ -4,3 +4,11 @@ print ("Hola, mundo")
 """este es un cmentario
 de varias lineas ya que es de tres comillas 
 y vaya que si funciona"""
+print ("Hola, mundo")
+print ("Hola, mundo")
+# Consultar el tipo de dato 
+print (type("Hola, mundo"))# Tipo "str"
+print (type(5)) # Tipo "int"
+print (type(1.5)) # Tipo "float"
+print (type(3 + 1j)) # Tipo "complex"
+print (type(True)) # Tipo "bool"
